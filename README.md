f121cf07bab80cd4d3400b1ab2104a666405c5af
Matthew Du Pont <1626189+mattdp@users.noreply.github.com>
2023-05-02 11:01:35 +0100

    printer methods for table and rows

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ DONE V1: no HTTP, just internal script logic making a queue of changes
 DONE V2: connect to a local DB that's blank or cluttered and make a single change
 V3: store queue of changes in a DB table + be able to access them in order
     DONE method to wipe entries in table
+    DONE method to print the table (added row printer)
+    method to print info of a set of sequential entries in a queue
     method to store set of tuples in table
     given a tuple, find the next tuple in sequence assuming it exists
     error if it doesn't exist
-    method to print info of a set of sequential entries in a queue
 V4: tests of what full logic should be able to do w/i DB - output is txt
 V5: get it off my machine and into the cloud, including a DB
 V6: HTTP inputs so can curl to it
--- main.py
+++ main.py
@@ -2,43 +2,55 @@ import sqlite3
 
 #underlying assumption: table called QUEUE
 
-connection = sqlite3.connect('first.db')
-
-connection.execute("DROP TABLE QUEUE")
-
-table_creation_text = '''
-CREATE TABLE IF NOT EXISTS QUEUE
-(CRM_ID      TEXT                    NOT NULL,
-PAYLOAD     TEXT                    NOT NULL,
-NEXT_ID     INT);
-'''
-
-connection.execute(table_creation_text)
-
-connection.execute("INSERT INTO QUEUE (CRM_ID,PAYLOAD,NEXT_ID) VALUES (8,'The first insertion',NULL)")
-
-cursor = connection.execute("SELECT * FROM QUEUE")
-for row in cursor:
-    print("CRM_ID = ", row[0])
-
-queue = [(1,"remove spanner"),(5,"reticulate splines"),(8,"polish vortices")]
-
 def main():
-    add_tuple((89,"sample addition"))
-    process_tuple()
-    wipe_table(connection,"QUEUE")
+    queue = [(1,"remove spanner"),(5,"reticulate splines"),(8,"polish vortices")]
+
+    add_tuple(queue,(89,"sample addition"))
+    process_tuple(queue)
     print(queue)
+
+    connection = sqlite3.connect('first.db')
+    table_name = "QUEUE"
+
+    wipe_table(connection,table_name)
+    #FIX: underlying assumption: table called QUEUE
+    connection.execute("INSERT INTO QUEUE (CRM_ID,PAYLOAD,NEXT_ID) VALUES (8,'The first insertion',NULL)")
+    print_table(connection,table_name)
+
     return 0
+
+#not called unless making a change to table structure
+#underlying assumption: table called QUEUE
+def initialize_schema():
+    connection.execute("DROP TABLE QUEUE")
+
+    table_creation_text = '''
+    CREATE TABLE IF NOT EXISTS QUEUE
+    (CRM_ID      TEXT                    NOT NULL,
+    PAYLOAD     TEXT                    NOT NULL,
+    NEXT_ID     INT);
+    '''
+
+    connection.execute(table_creation_text)
 
 # deliberately delete all entries from table
 def wipe_table(connection,table_name):
     connection.execute(f"DELETE FROM {table_name}")
 
-def add_tuple(change):
+def print_table(connection,table_name):
+    cursor = connection.execute("SELECT * FROM QUEUE")
+    for row in cursor:
+        print_row(row)
+
+def print_row(row):
+    strings = list(map(str,row))
+    print(",".join(strings))
+
+def add_tuple(queue,change):
     queue.append(change)
     return 0
 
-def process_tuple():
+def process_tuple(queue):
     return queue.pop(0)
 
 main()
